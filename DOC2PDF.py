c04966e82b4043207204c6013573e641f6741b6c
zh4men9 <zh4men9@163.com>
2022-08-22 17:08:36 +0800

    update

--- DOC2PDF.py
+++ DOC2PDF.py
@@ -3,6 +3,9 @@ from utils.merge_pages import merge_pages
 from utils.cut_pages import cut_pages
 from utils.merge_pdf import merge_pdf
 
+import tkinter
+from tkinter.messagebox import *
+
 
 def get_args():
     pass
@@ -30,16 +33,26 @@ def main():
     merge_pdf_output_path = 'merge'
     merge_pdf_output_file_name = 'merged_pdf'
 
-    # my_doc2pdf(input_path=doc2pdf_input_path, output_path=doc2pdf_output_path)
+    showinfo('提示', '正在进行文档转换PDF, 请稍等...')
 
-    # merge_pages(input_path=merge_pages_input_path, output_path=merge_pages_output_path,
-    # col_width=col_width)
+    my_doc2pdf(input_path=doc2pdf_input_path, output_path=doc2pdf_output_path)
 
-    # cut_pages(input_path='double_col', output_path='cut',
-    # top_margin=top_margin, bottom_margin=bottom_margin, width=width, height=height)
+    showinfo('提示', '文档转换PDF成功, 正在进行转换多栏, 请稍等...')
+
+    merge_pages(input_path=merge_pages_input_path, output_path=merge_pages_output_path,
+                col_width=col_width)
+
+    showinfo('提示', '多栏转换成功, 正在进行页边距裁减, 请稍等...')
+
+    cut_pages(input_path='double_col', output_path='cut',
+              left_margin=left_margin, right_margin=right_margin, top_margin=top_margin, bottom_margin=bottom_margin)
+
+    showinfo('提示', '页边距裁减成功, 正在进行文件合并, 请稍等...')
 
     merge_pdf(input_path=merge_pdf_input_path, output_path=merge_pdf_output_path,
               output_file_name=merge_pdf_output_file_name)
+
+    showinfo('提示', '文件合并成功, 程序执行结束, 正在退出...')
 
 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-"# DOC2PDF" 
+# DOC2PDF
+
+正如DOC2PDF名字可见，这个工具将DOC文档转换成PDF文件，批量转换，解放双手。详细功能如下：
+
+- 文档转换（doc -> pdf， docx -> pdf）
+- 多页合并为单页
+- 裁剪页边距
+- 合并文件
+
+更详细介绍见博客：[(10条消息) DOC2PDF项目博客_zh4men9的博客-CSDN博客](https://blog.csdn.net/qq_32614873/article/details/126469728?csdn_share_tail=%7B%22type%22%3A%22blog%22%2C%22rType%22%3A%22article%22%2C%22rId%22%3A%22126469728%22%2C%22source%22%3A%22qq_32614873%22%7D)
+
+## TO DO
+
+* [ ] 多页合并
+* [ ] 选择文件进行操作
